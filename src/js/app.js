6578f81392f284824da2ff2cba7a2c41d6e82a70
Brandonn Melchor <97859992+brandonnmelchor@users.noreply.github.com>
2022-04-25 14:38:45 -0500

    add event listeners to form fields

--- src/js/app.js
+++ src/js/app.js
@@ -2,22 +2,34 @@ import * as bootstrap from "bootstrap";
 
 let readingList = [];
 
-function Book(title, author, year, read) {
+function Book(title, author, year, comments, read) {
   this.title = title;
   this.author = author;
   this.year = year;
+  this.comments = comments;
   this.read = read;
   this.showInfo = function () {
     return `${title} by ${author}. Written ${year}. ${read ? "Read" : "Pending"}.`;
   };
 }
 
-function addBookToList(title, author, year, read) {
-  const newBook = new Book(title, author, year, read);
+function addBookToList(title, author, year, comments, read) {
+  const newBook = new Book(title, author, year, comments, read);
   readingList.push(newBook);
 }
 
-let title = "The Hobbit";
-let author = "J.R.R. Tolkien";
-let year = 295;
-let read = true;
+const title = document.querySelector("#title");
+const author = document.querySelector("#author");
+const year = document.querySelector("#year");
+const comments = document.querySelector("#comments");
+const read = "";
+
+const form = document.querySelector("#form-book");
+form.addEventListener("submit", submitBook);
+
+function submitBook() {
+  const newRead = document.querySelector('input[name="read-pending"]:checked');
+  read = newRead.value;
+}
+
+console.log(read);
