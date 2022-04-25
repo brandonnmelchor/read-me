2c0f5170231bfddf768cdb56097c6b15d6084ac7
Brandonn Melchor <97859992+brandonnmelchor@users.noreply.github.com>
2022-04-24 23:16:48 -0500

    creading readingList, Book(), addBookToList(), and book key/vals var

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,23 @@
+let readingList = [];
+
 function Book(title, author, pages, read, comments) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
   this.comments = comments;
-  this.info = function () {
+  this.showInfo = function () {
     return `${title} by ${author}. ${pages} pages. ${read ? "Already read" : "Not read yet"}. Info: ${comments}.`;
   };
 }
+
+function addBookToList(title, author, pages, read, comments) {
+  const newBook = new Book(title, author, pages, read, comments);
+  readingList.push(newBook);
+}
+
+let title = "The Hobbit";
+let author = "J.R.R. Tolkien";
+let pages = 295;
+let read = true;
+let comments = "Pretty good book";
